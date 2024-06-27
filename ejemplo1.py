68397e45c96a8121e7257932c984f68ef6863648
Rodrigo Valdivieso <vrodrigoaaron@gmail.com>
2024-06-27 20:14:05 -0300

    agrego funcion sum y rest en los archivos

---
+++ ejemplo1.py
@@ -0,0 +1,4 @@
+def restar(n1,n2):
+    print("El resultado de la resta entre n1 + n2 es de : ", n1 - n2)
+
+    restar(int(input("ingresa el primer numero")),int(input("ingresa el segundo numero")))
--- ejemplo2.py
+++ ejemplo2.py
@@ -2,3 +2,8 @@ def mensaje():
     print("estoy trabajando con git")
 
 mensaje()
+
+def sumar(n1,n2):
+    print("El resultado de la suma entre n1 + n2 es de : ", n1 + n2)
+
+    sumar(int(input("ingresa el primer numero")),int(input("ingresa el segundo numero")))
