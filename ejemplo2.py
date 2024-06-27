b7da1691bbdb17d613de975fd52fe4926af05338
Rodrigo Valdivieso <vrodrigoaaron@gmail.com>
2024-06-27 20:48:52 -0300

    estoy en la rama Rodri

--- ejemplo2.py
+++ ejemplo2.py
@@ -3,7 +3,13 @@ def mensaje():
 
 mensaje()
 
+def mensaje2():
+    print("volvi al pasado")
+
 def sumar(n1,n2):
     print("El resultado de la suma entre n1 + n2 es de : ", n1 + n2)
 
     sumar(int(input("ingresa el primer numero")),int(input("ingresa el segundo numero")))
+
+def mensaje3():
+    print("usando rama nueva Rodri")
